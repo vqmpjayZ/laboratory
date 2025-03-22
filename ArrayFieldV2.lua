6aa671e4f5028a0695c53e9244fa20a3e66aac25
vqmpjay <bakir.butkovic@osgrbavica2.edu.ba>
2025-03-22 14:28:00 +0100

    Update ArrayFieldV2.lua

--- ArrayFieldV2.lua
+++ ArrayFieldV2.lua
@@ -12,7 +12,7 @@ Arrays  | Designing + Programming + New Features
 
 
 
-local Release = "Release 2A" --0.9
+local Release = "Release 2A" --0.10
 local NotificationDuration = 6.5
 local ArrayFieldFolder = "ArrayField"
 local ConfigurationFolder = ArrayFieldFolder.."/Configurations"
