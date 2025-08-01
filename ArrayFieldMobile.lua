4383a21cc40d5dcf65990b82b5ff2d33abd024c5
vqmpjay <bakirbutkovic13@gmail.com>
2025-08-01 17:00:46 +0200

    Update ArrayFieldMobile.lua

--- ArrayFieldMobile.lua
+++ ArrayFieldMobile.lua
@@ -1,3 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/laboratory/refs/heads/main/ArrayFieldGen2.lua"))()
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/laboratory/refs/heads/main/ArrayFieldGen2.lua"))()
 -- changed loadstrings solely cz of a cooler name LMFAO. tons of new features out tho
 -- dsc.gg/vadriftz
