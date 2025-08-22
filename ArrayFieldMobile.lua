8929add20adae1515df59401e21b2482c053950b
vqmpjay <bakirbutkovic13@gmail.com>
2025-08-22 15:05:00 +0200

    Delete ArrayFieldMobile.lua

--- ArrayFieldMobile.lua
+++
@@ -1,3 +0,0 @@
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/laboratory/main/ArrayFieldGen2.lua"))()
--- changed loadstrings solely cz of a cooler name LMFAO. tons of new features out tho
--- dsc.gg/vadriftz
