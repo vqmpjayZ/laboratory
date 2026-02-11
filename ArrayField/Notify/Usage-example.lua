c657621c4fa321b566036b0fbde99fb0e0b96f47
vqmpjay <bakirbutkovic13@gmail.com>
2026-02-11 14:36:04 +0100

    Update Usage-example.lua

--- ArrayField/Notify/Usage-example.lua
+++ ArrayField/Notify/Usage-example.lua
@@ -1,15 +1,16 @@
-local Notify = loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/laboratory/refs/heads/main/ArrayField/Notify/NotificationModule.lua"))()
+local Notify --[[or call it whatever]] = loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/laboratory/refs/heads/main/ArrayField/Notify/NotificationModule.lua"))()
 
 Notify:Notify({
     Title = "Hello",
     Content = "This is a test notification",
     Duration = 5,
-    Image = "3944680095",
+    Image = "badge-check", -- or rbxassetid like 3944680095
 })
 
 Notify:Notify({
     Title = "With Actions",
     Content = "Pick one",
+    Image = "sword",
     Actions = {
         {Name = "Accept", Callback = function() print("Accepted") end},
         {Name = "Decline", Callback = function() print("Declined") end},
