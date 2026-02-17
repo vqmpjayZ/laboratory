c21ddae36265402f76051a1bae4dd642091fed81
vqmpjay <bakirbutkovic13@gmail.com>
2026-02-17 23:22:16 +0100

    Update and rename test2.lua to tests2.lua

--- test2.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/laboratory/refs/heads/main/test1.lua"))()
---
+++ tests2.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vqmpjayZ/laboratory/refs/heads/main/tests1.lua"))()
